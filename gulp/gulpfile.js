f09e896ed122df69a5a3d13cf886c1e526012c95
Vladyslav Karpenko <vladyslav@Mac-Vladyslav.local>
2019-02-27 19:41:26 +0200

    Add Fonts folder into the src & update Gulp Tasks

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -3,10 +3,12 @@ const { src, dest, series, parallel } = require("gulp");
 const   sourcemaps      =       require("gulp-sourcemaps"),
         plumber         =       require("gulp-plumber"),
         rename          =       require('gulp-rename'),
-        watch           =       require("gulp-watch");
+        watch           =       require("gulp-watch"),
+        fse             =       require('fs-extra'),
+        path            =       require('path');
 
 
-const path = {
+const path2files = {
     scss: '../src/scss/**/*.scss',
     css: '../build/css/',
     srcJs: '../src/js/**/*.js',
@@ -15,9 +17,62 @@ const path = {
     buildImages: '../build/images/',
     srcHTML: '../src/html/*.html',
     buildHTML: '../build/',
-    srcFonts: '../src/fonts/',
+    srcFonts: '../src/fonts/**/*',
     buildFonts: '../build/fonts/',
 
+};
+
+const copyFiles = function copyFiles(files) {
+    const copy = (file, dest) => {
+        fse.ensureDir(dest);
+        return fse.copy(file, dest, err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`Successfully copied file ${file}!`);
+        });
+    };
+    files.forEach((file) => copy(
+        file.from,
+        `${file.dest}${path.basename(file.from)}`
+    ));
+};
+
+const buildLib = function buildModules() {
+
+    /* const modules = [
+        {
+            from: '../node_modules/jquery/dist/jquery.min.js',
+            dest: '../build/lib/jquery/',
+        },
+        {
+            from: '../node_modules/bootstrap/dist/css/bootstrap.min.css',
+            dest: '../build/lib/bootstrap/v4/',
+        },
+        {
+            from: '../node_modules/bootstrap/dist/css/bootstrap.min.css.map',
+            dest: '../build/lib/bootstrap/v4/',
+        }
+    ];
+    fse.ensureDir('./build/lib/')
+        .then(() => {
+            copyFiles(modules);
+        })
+        .catch(err => {
+            console.error(err)
+        }); */
+
+};
+
+function fontsTask(cb) {
+    fse.ensureDir(path2files.buildFonts);
+    src(path2files.srcFonts)
+      .pipe(dest(path2files.buildFonts))
+      .on('end', () => {
+        console.log('Successfully copy Fonts');
+      });
+    cb();
 };
 
 function scss(cb) {
@@ -26,8 +81,8 @@ function scss(cb) {
     const cssnano         =       require("cssnano");
     const sass            =       require('gulp-sass');
 
-    src(path.scss)
-       .pipe(watch(path.scss))
+    src(path2files.scss)
+       .pipe(watch(path2files.scss))
        .pipe(plumber())
        .pipe(sourcemaps.init())
        .pipe(sass({
@@ -35,22 +90,22 @@ function scss(cb) {
        }))
        .pipe( postcss([ autoprefixer() ]) )
        .pipe(sourcemaps.write())
-       .pipe(dest(path.css))
+       .pipe(dest(path2files.css))
        .pipe( postcss([ cssnano() ]) )
        .pipe(rename({suffix: '.min'}))
-       .pipe(dest(path.css));
+       .pipe(dest(path2files.css));
     cb();
 }
 
 function js(cb) {
     const uglify = require("gulp-uglify-es").default;
 
-    src(path.srcJs)
-        .pipe(watch(path.srcJs))
+    src(path2files.srcJs)
+        .pipe(watch(path2files.srcJs))
         .pipe(plumber())
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(dest(path.buildJs));
+        .pipe(dest(path2files.buildJs));
     cb();
 }
 
@@ -58,11 +113,11 @@ function js(cb) {
 function html(cb) {
     const twig    = require("gulp-twig");
 
-    src([path.srcHTML])
-      .pipe(watch(path.srcHTML))
+    src([path2files.srcHTML])
+      .pipe(watch(path2files.srcHTML))
       .pipe(plumber())
       .pipe(twig())
-      .pipe(dest(path.buildHTML));
+      .pipe(dest(path2files.buildHTML));
     cb();
 }
 
@@ -71,12 +126,12 @@ function html_min(cb) {
     const twig    = require("gulp-twig");
     const htmlmin = require('gulp-htmlmin');
 
-    src([path.srcHTML])
-      .pipe(watch(path.srcHTML))
+    src([path2files.srcHTML])
+      .pipe(watch(path2files.srcHTML))
       .pipe(plumber())
       .pipe(twig())
       .pipe(htmlmin({ collapseWhitespace: true }))
-      .pipe(dest(path.buildHTML));
+      .pipe(dest(path2files.buildHTML));
     cb();
 }
 
@@ -84,19 +139,19 @@ function html_min(cb) {
 function img(cb) {
     const imagemin = require('gulp-imagemin');
 
-    src(path.srcImages)
+    src(path2files.srcImages)
        .pipe(imagemin({
            optimizationLevel: 3,
            progressive: true,
            svgoPlugins: [{removeViewBox: false}],
            interlaced: true
        }))
-       .pipe(dest(path.buildImages));
+       .pipe(dest(path2files.buildImages));
     cb();
 }
 
 /*
-* After using the image optimization task "img" you should clean the folder src/image by task: 
+* After using the image optimization task "img" you may clean the folder src/image by task: 
 * "cleanSrcImg"
 * */
 function cleanSrcImg() {
@@ -111,7 +166,15 @@ exports.js       = js;
 exports.html     = html;
 exports.html_min = html_min;
 exports.img      = img;
+exports.fonts    = fontsTask;
+exports.buildLib    = buildLib;
 exports.cleanSrcImg = cleanSrcImg;
+
+exports.dev = series(
+    scss, 
+    js, 
+    html 
+);
 
 exports.default = series(
     scss, 
