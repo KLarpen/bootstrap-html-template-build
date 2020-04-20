02a6a8f8423ead8e9995a803cc21881268e2d666
Vladyslav Karpenko <klarpen@gmail.com>
2020-04-20 20:01:52 +0300

    Refactoring & npm packages update

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# bootstrap-html-template-build
+# Template build with HTML/TwigJS, SCSS, JS, images
+
+## Processing
+Template uses the [Gulp](https://gulpjs.com/) task runner. Check the all task`s names by executing ```gulp --tasks``` in the terminal. The tasks might be called by ```gulp <task-name>```. Alternatively you may call tasks by scripts alias in package.json as  ```npm run <script-name>```:
+* ```start``` – run default task wich will process sass, js, html and will continue to watch changes;
+* ```build``` – update the build folder by executing all sass, js, html processing, optimize & copy images, copy fonts and 3rd party libraries;
+* ```sass``` – process scss files once;
+* ```js``` – process js files once;
+* ```html``` – process html/twig files once (without minification);
+* ```html_min``` – process and minify html/twig files once;
+* ```img``` – optimize images and save it into build folder;
+* ```fonts``` – copy fonts to the build folder;
+* ```lib``` – copy 3rd party libraries (js, css, etc.), the project dependent on, from the node_modules to the build folder.
+* ```cleanSrcImg``` – remove all images from the src folder (Caution! You can not rebuild the images after clearing the source!);
+
+## Structure
+All project's source files placed in the ```src``` folder, specifically in the typed subfolders:
+* ```src/html```
+* ```src/scss```
+* ```src/js```
+* ```src/images```
+* ```src/fonts```
+
+The ```build``` folder structure are quite diferent:
+* all html files will be placed in the root of the directory;
+* ```build/css```
+* ```build/js```
+* ```build/images```
+* ```build/lib``` folder will contain frontend dependency 3rd party libraries` distributive files copied from the ```node_modules```.
+
+The workflow assumes that tasks do not clean the ```build``` folder. It might be even saved in the VCS repository. In case that's needed remove the folder using ```rm -R ./build```.
+
+## HTML templating engine
+The project uses [TwigJS](https://github.com/twigjs/twig.js) HTML templating engine. The task runner processes ```.twig``` files as well as files with normal ```.html``` extension. The build folder will get the processed result of the all root level files from the ```./src/html``` folder! All source partials and includes must be placed in subfolders, e.g. ```./src/html/template-parts```.
---
+++ gulpfile.js
@@ -0,0 +1,196 @@
+"use strict";
+const { src, dest, series, parallel, watch } = require("gulp");
+const   sourcemaps    = require("gulp-sourcemaps"),
+        plumber       = require("gulp-plumber"),
+        rename        = require('gulp-rename'),
+        fse           = require('fs-extra'),
+        path          = require('path'),
+        uglify        = require("gulp-uglify-es").default,
+        postcss       = require('gulp-postcss'),
+        autoprefixer  = require("autoprefixer"),
+        cssnano       = require("cssnano"),
+        sass          = require('gulp-sass'),
+        twig          = require("gulp-twig");
+
+
+const path2files = {
+    scss: './src/scss/**/*.scss',
+    css: './build/css/',
+    srcJs: './src/js/**/*.js',
+    buildJs: './build/js/',
+    srcImages : './src/images/**/*.*',
+    buildImages: './build/images/',
+    srcHTML: './src/html/*.html',
+    buildHTML: './build/',
+    srcFonts: './src/fonts/**/*',
+    buildFonts: './build/fonts/',
+
+};
+const libs = [
+    {
+        from: './node_modules/jquery/dist/',
+        dest: './build/lib/jquery/',
+    },
+    {
+        from: './node_modules/bootstrap/dist/',
+        dest: './build/lib/bootstrap/',
+    },
+    {
+        from: './node_modules/bs-custom-file-input/dist',
+        dest: './build/lib/plugins/',
+    }
+];
+
+const copyFiles = function (files, cb) {
+    const copy = (file, dest) => {
+        fse.ensureDir(dest);
+        return fse.copy(file, dest)
+        .then(() => {
+            console.log(`Successfully copied ${file}`);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+        
+    };
+    let result = files.map((file) => copy(
+        file.from,
+        file.dest
+    ));
+    Promise.all(result).then( () => cb() );
+};
+
+function buildLib(cb) {
+    fse.ensureDir('./build/lib/')
+        .then(() => {
+            copyFiles(libs, cb);
+        })
+        .catch(err => {
+            console.error(err)
+        });
+};
+
+function fontsTask(cb) {
+    fse.ensureDir(path2files.buildFonts);
+    src(path2files.srcFonts)
+      .pipe(dest(path2files.buildFonts))
+      .on('end', () => {
+        console.log('Fonts successfully copied');
+        cb();
+      });
+};
+
+function sassTask(cb) {
+    fse.ensureDir(path2files.css);
+    src(path2files.scss)
+       .pipe(plumber())
+       .pipe(sourcemaps.init())
+       .pipe(sass({
+           sourceComments: 'normal'
+       }))
+       .pipe( postcss([ autoprefixer() ]) )
+       .pipe(sourcemaps.write())
+       .pipe(dest(path2files.css))
+       .pipe( postcss([ cssnano() ]) )
+       .pipe(rename({suffix: '.min'}))
+       .pipe(dest(path2files.css))
+       .on('end', () => { cb(); });
+}
+
+function js(cb) {
+    fse.ensureDir(path2files.buildJs);
+    src(path2files.srcJs)
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest(path2files.buildJs))
+        .on('end', () => { cb(); });
+}
+
+/* HTML templating based on TwigJS. */
+function html(cb) {
+    fse.ensureDir(path2files.buildHTML);
+    src([path2files.srcHTML])
+      .pipe(plumber())
+      .pipe(twig())
+      .pipe(dest(path2files.buildHTML))
+      .on('end', () => { cb(); });
+}
+
+/* The same HTML templating with minification based on htmlmin. */
+function html_min(cb) {
+    const htmlmin = require('gulp-htmlmin');
+
+    fse.ensureDir(path2files.buildHTML);
+    src([path2files.srcHTML])
+      .pipe(plumber())
+      .pipe(twig())
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(dest(path2files.buildHTML))
+      .on('end', () => { cb(); });
+}
+
+
+function img(cb) {
+    const imagemin = require('gulp-imagemin');
+
+    fse.ensureDir(path2files.buildImages);
+    src(path2files.srcImages)
+       .pipe(imagemin({
+           optimizationLevel: 3,
+           progressive: true,
+           svgoPlugins: [{removeViewBox: false}],
+           interlaced: true
+       }))
+       .pipe(dest(path2files.buildImages))
+       .on('end', () => { cb(); });
+}
+
+/*
+* After using the image optimization task "img" you may clean the folder src/image by task: 
+* "cleanSrcImg"
+* */
+function cleanSrcImg(cb) {
+    const del = require("del");
+    return del(['./src/images/**/*', '!./src/images', '!./src/images/.gitkeep', '!./src/images/icons', '!./src/images/icons/.gitkeep'], {force: true})
+        .then(paths => {
+            console.log('Deleted: ', paths);
+            cb();
+        });
+}
+
+function watchChanges() {
+    watch([ './src/html/**/*.html' ], html);
+    watch(path2files.scss, sassTask);
+    watch(path2files.srcJs, js);
+}  
+
+// Make tasks public
+exports.sass     = sassTask;
+exports.js       = js;
+exports.html     = html;
+exports.html_min = html_min;
+exports.img      = img;
+exports.fonts    = fontsTask;
+exports.lib      = buildLib;
+exports.cleanSrcImg = cleanSrcImg;
+
+exports.default = series(
+    parallel(
+        sassTask, 
+        js, 
+        html
+    ),
+    watchChanges
+);
+
+exports.build = series(
+    html,
+    fontsTask,
+    buildLib,
+    parallel(
+        sassTask, 
+        js, 
+        img
+    )
+);
